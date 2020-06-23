3870f3a4d9d34ae4f166f992aa23aa2a521b8bdb
ibwharri <ibwharri@mit.edu>
2020-06-23 18:39:09 -0400

    Added plot of S matrix

--- LinearStrainSweep.py
+++ LinearStrainSweep.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
+from matplotlib.cm import get_cmap
+from matplotlib.colors import Normalize
 import numpy as np
 from Simulation import *
 
@@ -21,7 +23,7 @@ B = B_magnitude*np.array( [np.cos(theta), np.sin(theta)] )
 max_strain = 20 # GHz
 Npts = 200
 beta = np.linspace(-max_strain, max_strain, Npts)
-Npts = 11
+Npts = 5
 alpha = np.linspace(-max_strain, max_strain, Npts)
 alpha, beta = np.meshgrid( alpha, beta)
 
@@ -37,12 +39,32 @@ T2_x_corr = thermalisedOrbitalDephasingRate(H, Lcorr, kBT, 'x')
 D = getDephasingOperator(Lcorr, 'x')
 Mu = getDephasingComponents(D)
 
-plt.subplot(211)
+plt.subplot(311)
+cmap = get_cmap('viridis')
+norm = Normalize()
+norm.autoscale(alpha)
 for i,strain in enumerate(alpha[0,:]):
-    plt.plot(beta[:,0], T2_x_corr[:,i], label='{0:.1f}'.format(strain))
+    plt.plot(beta[:,0], T2_x_corr[:,i], label='{0}={1:.1f}'.format(r'$\alpha$', strain), color=cmap(norm(strain)))
 plt.legend()
+plt.ylabel(r'$T_2^{-1}$ (arb.)')
+
+plt.subplot(312)
+S = np.kron(z, I) + np.kron(x, I)
+S = np.einsum('...ji,...jk,...kl->...il', np.conjugate(V), S, V)
+Mu = np.real(getDephasingComponents(S))
+
 for i,strain in enumerate(alpha[0,:]):
-    plt.subplot(int(np.ceiling(Npts/5)), 5)
-    plt.imshow(Mu[i,])
+    plt.plot(beta[:,0], Mu[:,i,0,1], label='{0}={1:.1f}'.format(r'$\alpha$', strain), color=cmap(norm(strain)))
+plt.ylabel(r'$Tr(\sigma_0\otimes\sigma_xS)$ (GHz)')
+plt.legend()
+
+plt.subplot(313)
+for i,strain in enumerate(alpha[0,:]):
+    plt.plot(beta[:,0], np.sqrt(beta[:,0]**2+alpha[:,i]**2), label='{0}={1:.1f}'.format(r'$\alpha$', strain), color=cmap(norm(strain)))
+    #plt.plot(beta[:,0], E[:,i,1], color=cmap(norm(strain)))
+    #plt.plot(beta[:,0], E[:,i,2], color=cmap(norm(strain)))
+    #plt.plot(beta[:,0], E[:,i,3], color=cmap(norm(strain)))
+plt.xlabel(r'$\beta$ (GHz)')
+plt.ylabel(r'E (GHz)')
 plt.legend()
 plt.show()
